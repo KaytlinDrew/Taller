3676fbd7e3177886d69ee7ef02d66d452f7dd3d3
unknown <kaytlin.espinoza@pucp.edu.pe>
2019-03-30 11:20:05 -0500

    version2.0

--- archivo.c
+++ archivo.c
@@ -6,10 +6,12 @@ int main(){
 		if((i%3)==0)
 			printf("%d ",i);
 	}
+	printf("\n");
 	for(int i=1;i<101;i++){
 		if((i%5)==0)
 			printf("%d ",i);
 	}
+	printf("\n");
 	for(int i=1;i<101;i++){
 		if(((i%5)==0)&&((i%3)==0))
 			printf("%d ",i);
